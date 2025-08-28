bbf122192152530bcf23f659fd34491d3f06c680
EpicNNG <EpicNNG@gmail.com>
2025-08-28 01:04:47 +0100

    Update EpicEffects.py

--- CrowdControl/EpicEffects.py
+++ CrowdControl/EpicEffects.py
@@ -1,6 +1,5 @@
 import unrealsdk
 import random
-import math
 from mods_base import get_pc, ENGINE
 from unrealsdk import find_object, make_struct, find_all, find_class
 from ui_utils import show_hud_message
@@ -14,8 +13,7 @@ class HighsLows(Effect):
     def highsandlows(self):
         self.pc.Pawn.K2_GetActorLocation()
         self.pc.Pawn.K2_GetActorRotation()
-        loc = make_struct("Vector", X=get_pc().pawn.K2_GetActorLocation().X + 500 * math.cos(math.radians((get_pc().pawn.K2_GetActorRotation().Yaw))), Y=get_pc().pawn.K2_GetActorLocation().Y + 500 * math.sin(math.radians((get_pc().pawn.K2_GetActorRotation().Yaw))), Z=get_pc().pawn.K2_GetActorLocation().Z - 112)
-        rot = make_struct("Rotator", Roll =0, Pitch=0, Yaw=get_pc().pawn.K2_GetActorRotation().Yaw-180)
-        print(loc)
+        loc = make_struct("Vector", X=self.pc.pawn.K2_GetActorLocation().X, Y=self.pc.pawn.K2_GetActorLocation().Y, Z=self.pc.pawn.K2_GetActorLocation().Z + random.randrange(-10000, 75000))
+        rot = self.pc.pawn.K2_GetActorRotation()
         self.pc.Pawn.K2_TeleportTo(loc,rot)
         return super().run_effect()
