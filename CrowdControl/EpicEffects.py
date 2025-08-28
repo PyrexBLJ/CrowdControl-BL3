d66135cfea3ad2f20bf0a6477c8f00a901e4812d
EpicNNG <EpicNNG@gmail.com>
2025-08-28 02:31:42 +0100

    Update EpicEffects.py

--- CrowdControl/EpicEffects.py
+++ CrowdControl/EpicEffects.py
@@ -10,10 +10,13 @@ from .Effect import Effect
 class HighsLows(Effect):
     effect_name = "HighsLows"
     display_name = "Highs And Lows"
-    def highsandlows(self):
-        self.pc.Pawn.K2_GetActorLocation()
-        self.pc.Pawn.K2_GetActorRotation()
-        loc = make_struct("Vector", X=self.pc.pawn.K2_GetActorLocation().X, Y=self.pc.pawn.K2_GetActorLocation().Y, Z=self.pc.pawn.K2_GetActorLocation().Z + random.randrange(-10000, 75000))
-        rot = self.pc.pawn.K2_GetActorRotation()
-        self.pc.Pawn.K2_TeleportTo(loc,rot)
+    def run_effect(self):
+        if AmIHost():
+            self.pc.Pawn.K2_GetActorLocation()
+            self.pc.Pawn.K2_GetActorRotation()
+            loc = make_struct("Vector", X=self.pc.pawn.K2_GetActorLocation().X, Y=self.pc.pawn.K2_GetActorLocation().Y, Z=self.pc.pawn.K2_GetActorLocation().Z + random.randrange(-10000, 75000))
+            rot = self.pc.pawn.K2_GetActorRotation()
+            self.pc.Pawn.K2_TeleportTo(loc,rot)
+        else:
+            get_pc().ServerChangeName(f"CrowdControl-{get_pc().PlayerState.PlayerID}-HighsLows-{self.id}")
         return super().run_effect()
