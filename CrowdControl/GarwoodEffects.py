156f2264e574b54c8f50cb6cb5046bb9108acad6
Garw00d <132993341+Garw00d@users.noreply.github.com>
2025-09-01 04:54:24 +0100

    Update GarwoodEffects.py

--- CrowdControl/GarwoodEffects.py
+++ CrowdControl/GarwoodEffects.py
@@ -5,7 +5,7 @@ from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct
 from unrealsdk.hooks import Type, add_hook, remove_hook
 import math
 from unrealsdk import make_struct
-from .Utils import SpawnInteractiveObject,AmIHost
+from .Utils import SpawnInteractiveObject,AmIHost,SendToHost
 
 class SuperHot(Effect):
 
